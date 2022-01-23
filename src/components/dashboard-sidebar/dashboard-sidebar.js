351cbc8b3e1e4b10dea512157d38a0047ed77ee5
Marcelo Lopes de Souza <marcelosouza323@gmail.com>
2022-01-22 22:52:58 -0300

    corrigido erro  de exportacao

--- src/components/dashboard-sidebar/dashboard-sidebar.js
+++ src/components/dashboard-sidebar/dashboard-sidebar.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
-import Logo from '../../img/logo.png';
-import '../dashboard-sidebar/dashboard-sidebar.css';
+import React, { useEffect } from "react";
+import Logo from "../../img/logo.png";
+import "../dashboard-sidebar/dashboard-sidebar.css";
 import {
   FaDelicious,
   FaShoppingCart,
@@ -9,30 +9,30 @@ import {
   FaRegClock,
   FaCog,
   FaSignOutAlt,
-} from 'react-icons/fa';
+} from "react-icons/fa";
 
-function DashboardSidebar() {
+export function DashboardSidebar() {
   useEffect(() => {
-    const mainMenuLi = document.getElementById('mainMenu').querySelector('li');
+    const mainMenuLi = document.getElementById("mainMenu").querySelector("li");
 
     function changeActive() {
-      mainMenuLi.forEach((n) => n.classList.remove('active'));
-      this.classList.add('active');
+      mainMenuLi.forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
     }
 
-    mainMenuLi.forEach((n) => n.addEventListener('click', changeActive));
+    mainMenuLi.forEach((n) => n.addEventListener("click", changeActive));
   }, []);
   return (
     <menu>
-      <img src={Logo} alt='' />
-      <ul id='mainMenu'>
+      <img src={Logo} alt="" />
+      <ul id="mainMenu">
         <Icon icon={<FaDelicious />} />
         <Icon icon={<FaShoppingCart />} />
         <Icon icon={<FaWallet />} />
         <Icon icon={<FaChartLine />} />
         <Icon icon={<FaRegClock />} />
       </ul>
-      <ul className='settingsMenu'>
+      <ul className="settingsMenu">
         <Icon icon={<FaCog />} />
         <Icon icon={<FaSignOutAlt />} />
       </ul>
@@ -43,6 +43,6 @@ function DashboardSidebar() {
 // OS ÍCONES ESTÃO SENDO PASSADOS POR PROPS.
 const Icon = ({ icon }) => {
   <li>
-    <a href='#'>{icon}</a>
+    <a href="#">{icon}</a>
   </li>;
 };

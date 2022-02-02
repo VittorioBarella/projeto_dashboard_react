d609f86ed41352ac49a9a4e97f51173cd45d1423
VittorioBarella <vittorio.br@hotmail.com>
2022-02-02 09:27:14 -0300

    changes on navbar and sidebar

--- src/components/dashboard-navbar/dashboard-navbar.js
+++ src/components/dashboard-navbar/dashboard-navbar.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Logo from '../../img/logo.png';
 import './dashboard-navbar.css';
 function DashboardNavbar() {
   return (
-    <nav className='navbar navbar-light bg-light justify-content-between w-100 fixed-top'>
+    <nav className='navbar justify-content-between w-100 fixed-top'>
       <form className='form-inline'>
+        <img src={Logo} alt='website-logo' className='webSiteLogo' />
         <input
           className='form-control mr-sm-2'
           type='search'

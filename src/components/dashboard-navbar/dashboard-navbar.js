32951b747f606255149f75af5eefbf99e5b77ac0
VittorioBarella <vittorio.br@hotmail.com>
2022-02-08 20:05:00 -0300

    add rows and columns at the navbar component

--- src/components/dashboard-navbar/dashboard-navbar.js
+++ src/components/dashboard-navbar/dashboard-navbar.js
@@ -4,32 +4,63 @@ import UserProfileImage from '../../img/user_image.jpg';
 import './dashboard-navbar.css';
 function DashboardNavbar() {
   return (
-    <nav className='navbar justify-content-between w-100 fixed-top'>
-      <form className='form-inline'>
-        <img src={Logo} alt='website-logo' className='webSiteLogo' />
-        <input
-          className='form-control mr-sm-2'
-          type='search'
-          placeholder='Pesquisar'
-          aria-label='Pesquisar'
-        />
-        <button className='btn btn-outline-success my-2 my-sm-0' type='submit'>
-          Pesquisar
-        </button>
-      </form>
-      <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <img src={UserProfileImage} width="30" height="30" className="d-inline-block align-top userProfileImage" alt="" />
-          <span className='text-light'>Vittório Andrade</span>
-        </a>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item" href="#">Action</a>
-          <a className="dropdown-item" href="#">Another action</a>
-          <div className="dropdown-divider"></div>
-          <a className="dropdown-item" href="#">Something else here</a>
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <nav className='navbar justify-content-between w-100 fixed-top'>
+            <form className='form-inline'>
+              <img src={Logo} alt='website-logo' className='webSiteLogo' />
+              <input
+                className='form-control mr-sm-2'
+                type='search'
+                placeholder='Pesquisar'
+                aria-label='Pesquisar'
+              />
+              <button
+                className='btn btn-outline-success my-2 my-sm-0'
+                type='submit'
+              >
+                Pesquisar
+              </button>
+            </form>
+            <div className='col d-flex justify-content-end'>
+              <li className='nav-item dropdown'>
+                <a
+                  className='nav-link dropdown-toggle'
+                  href='#'
+                  id='navbarDropdown'
+                  role='button'
+                  data-toggle='dropdown'
+                  aria-haspopup='true'
+                  aria-expanded='false'
+                >
+                  <img
+                    src={UserProfileImage}
+                    width='30'
+                    height='30'
+                    className='d-inline-block align-top userProfileImage'
+                    alt=''
+                  />
+                  <span className='text-light'>Vittório Andrade</span>
+                </a>
+                <div className='dropdown-menu' aria-labelledby='navbarDropdown'>
+                  <a className='dropdown-item' href='#'>
+                    Action
+                  </a>
+                  <a className='dropdown-item' href='#'>
+                    Another action
+                  </a>
+                  <div className='dropdown-divider'></div>
+                  <a className='dropdown-item' href='#'>
+                    Something else here
+                  </a>
+                </div>
+              </li>
+            </div>
+          </nav>
         </div>
-      </li>
-    </nav>
+      </div>
+    </div>
   );
 }
 

0640a66d844dec74de8a49491c5be4c86140bb68
vittorio.barella <vittorio.barella@cwi.com.br>
2022-02-14 16:25:59 -0300

    stylized the CardEvent

--- src/components/dashboard/event-card/events.js
+++ src/components/dashboard/event-card/events.js
@@ -4,14 +4,14 @@ import './event-card.css';
 export function CardEvent() {
   return (
     <div className='card text-center event-card'>
-      <div className='card-header'>Featured</div>
+      <div className='card-header text-white header-style'>Featured</div>
       <div className='card-body'>
         <h5 className='card-title'>Special title treatment</h5>
         <p className='card-text'>
           With supporting text below as a natural lead-in to additional content.
         </p>
       </div>
-      <div className='card-footer text-muted'>2 days ago</div>
+      <div className='card-footer text-white footer-style'>2 days ago</div>
     </div>
   );
 }
